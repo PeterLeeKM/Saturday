726ac3209a95bbe1888105cf668802640d18ad37
Peter Lee <luineleth@naver.com>
2014-02-28 20:50:36 +0900

    Register implemented with server

--- app/src/main/java/com/example/app/MemberManager.java
+++ app/src/main/java/com/example/app/MemberManager.java
@@ -1,6 +1,24 @@
 package com.example.app;
 
+import android.os.AsyncTask;
 import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParamBean;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,7 +28,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class MemberManager {
     private static MemberManager instance = null;
@@ -18,37 +40,12 @@ public class MemberManager {
     public static String path = Environment.getExternalStorageDirectory()+"/members.txt";
 
     private MemberManager() {
-        this.members = this.getMembersFromFile();
+        this.members = new ArrayList<Member>();
     }
     public static MemberManager getInstance() {
         if(MemberManager.instance == null)
             MemberManager.instance = new MemberManager();
         return MemberManager.instance;
-    }
-
-    private ArrayList<Member> getMembersFromFile() {
-
-        File source = new File(path);
-        BufferedReader reader = null;
-        ArrayList<Member> members = null;
-        try {
-            reader = new BufferedReader(new FileReader(source));
-            members = new ArrayList<Member>();
-            String content;
-            String [] data = null;
-            while(reader.ready()) {	// 변경
-                content = reader.readLine();
-                data = content.split("\t");
-                Member member = new Member(data[0], data[1], data[2], data[3], Integer.parseInt(data[4]));
-                members.add(member);
-            }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return members;
     }
 
     public Member findMember(String username){
@@ -61,21 +58,48 @@ public class MemberManager {
     }
 
     public void registerMember(Member member){
-        writeNewMemberOnFile(member);
-        this.members = getMembersFromFile();
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("username", member.getUsername()));
+        nameValuePairs.add(new BasicNameValuePair("password", member.getPassword()));
+        nameValuePairs.add(new BasicNameValuePair("groupName", member.getGroupName()));
+        nameValuePairs.add(new BasicNameValuePair("phoneNumber", member.getPhoneNumber()));
+        nameValuePairs.add(new BasicNameValuePair("groupMemberCount", String.valueOf(member.getGroupMemberCount())));
+        AsyncHttpPost asyncHttpPost = new AsyncHttpPost(nameValuePairs);
+        asyncHttpPost.execute("http://1.234.88.155:9338/register/");
     }
 
-    private void writeNewMemberOnFile(Member member) {
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(path, true)));
-            out.println(member.getUsername() + "\t" +
-                    member.getPassword() + "\t" +
-                    member.getGroupName() + "\t" +
-                    member.getPhoneNumber() + "\t" +
-                    String.valueOf(member.getGroupMemberCount()));
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public class AsyncHttpPost extends AsyncTask<String, String, String> {
+        private ArrayList<NameValuePair> nameValuePairs = null;
+
+        public AsyncHttpPost(ArrayList<NameValuePair> nameValuePairs) {
+            this.nameValuePairs = nameValuePairs;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String str = "";
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(params[0]);// in this case, params[0] is URL
+            try {
+                post.setEntity(new UrlEncodedFormEntity(this.nameValuePairs, "UTF-8"));
+                HttpResponse response = client.execute(post);
+                Log.w("response11", response.toString());
+            }
+            catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+            }
+            return str;
+        }
+
+        /**
+         * on getting result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            Log.w("response", result);
         }
     }
 }
+
--- app/src/main/java/com/example/app/SignupActivity.java
+++ app/src/main/java/com/example/app/SignupActivity.java
@@ -29,11 +29,14 @@ public class SignupActivity extends ActionBarActivity {
         }
         public void onClick(DialogInterface dialog, int which) throws NullPointerException{
             MemberManager memberManager = MemberManager.getInstance();
+            Log.i("register", "11111111");
             username = ((EditText)findViewById(R.id.edit_ID)).getText().toString();
+            Log.i("register", "22222222");
             password = ((EditText)findViewById(R.id.edit_password)).getText().toString();
             groupName = ((EditText)findViewById(R.id.edit_group_name)).getText().toString();
             phoneNumber = ((EditText)findViewById(R.id.edit_phone_number)).getText().toString();
             groupMemberCount = ((EditText)findViewById(R.id.edit_number_of_member)).getText().toString();
+            Log.i("register", "33333333");
 
             memberManager.registerMember(new Member(username, password, groupName, phoneNumber, Integer.parseInt(groupMemberCount)));
             Intent intent = new Intent(signup, MainActivity2.class);
@@ -74,12 +77,25 @@ public class SignupActivity extends ActionBarActivity {
     }
 
     public void signUp(View view){
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("알림");
+//        builder.setMessage("가입 완료!");
+//        builder.setNeutralButton("확인", new CommitDialog(this));
+//        builder.show();
+        MemberManager memberManager = MemberManager.getInstance();
+        username = ((EditText)findViewById(R.id.edit_ID)).getText().toString();
+        password = ((EditText)findViewById(R.id.edit_password)).getText().toString();
+        groupName = ((EditText)findViewById(R.id.edit_group_name)).getText().toString();
+        phoneNumber = ((EditText)findViewById(R.id.edit_phone_number)).getText().toString();
+        groupMemberCount = ((EditText)findViewById(R.id.edit_number_of_member)).getText().toString();
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("알림");
-        builder.setMessage("가입 완료!");
-        builder.setNeutralButton("확인", new CommitDialog(this));
-        builder.show();
+        memberManager.registerMember(new Member(username, password, groupName, phoneNumber, Integer.parseInt(groupMemberCount)));
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("username", username);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
         this.onStop();
     }
 
